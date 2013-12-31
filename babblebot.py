456eb0a0db62d03849f7f37080f494befd1fb0a8
Sebastian Ullrich <sebasti@nullri.ch>
2013-12-31 11:23:25 +0100

    Save babble sources in plugin storage instead of config, since the latter is not mutable from within the plugin

--- babblebot.py
+++ babblebot.py
@@ -11,25 +11,30 @@ from errbot import BotPlugin, botcmd
 
 
 class BabbleBot(BotPlugin):
+	def activate(self):
+		super().activate()
+		if 'sources' not in self:
+			self['sources'] = ['http://www.gutenberg.org/cache/epub/2229/pg2229.txt']
+		for error in self.reload():
+			logging.warn(error)
+
 	def configure(self, configuration):
 		super().configure(configuration)
 		if self.config is None:
 			self.config = self.get_configuration_template()
-		for error in self.reload():
-			logging.warn(error)
 
 	def get_configuration_template(self):
-		return {'NGRAM_N': 3, 'SOURCES': ['http://www.gutenberg.org/cache/epub/2229/pg2229.txt']}
+		return {'NGRAM_N': 3}
 
 	def check_configuration(self, configuration):
 		super().check_configuration(configuration)
 
 	def reload(self):
 		lines = []
-		for source in self.config['SOURCES']:
+		for source in self['sources']:
 			try:
 				f = urllib.request.urlopen(source)
-			except urllib.error.URLError:
+			except:
 				yield "Couldn't retrieve babble source: " + source
 				continue
 			with f:
@@ -53,18 +58,22 @@ class BabbleBot(BotPlugin):
 	def babble_sources(self, mess, args):
 		"""Lists all babble sources."""
 		return "\n".join("{} {}".format(idx, source) for idx, source in
-				enumerate(self.config['SOURCES']))
+				enumerate(self['sources']))
 
 	@botcmd
 	def babble_sources_add(self, mess, args):
 		"""Adds a URL as a new babble source."""
-		self.config['SOURCES'].append(args)
+		sources = self['sources']
+		sources.append(args)
+		self['sources'] = sources
 		for msg in self.babble_reload(mess, args):
 			yield msg
 
 	@botcmd
 	def babble_sources_remove(self, mess, args):
 		"""Removes the babble source with the given index."""
-		del self.config['SOURCES'][int(args)]
+		sources = self['sources']
+		del sources[int(args)]
+		self['sources'] = sources
 		for msg in self.babble_reload(mess, args):
 			yield msg
